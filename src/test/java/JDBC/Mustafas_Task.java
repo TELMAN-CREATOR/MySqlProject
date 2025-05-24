d61eedddf10a0261cd811280bc92d2d1ca14a199
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-05-24 15:35:40 +0300

    Projem bitti onaylar mısın?

--- src/test/java/JDBC/Mustafas_Task.java
+++ src/test/java/JDBC/Mustafas_Task.java
@@ -1,4 +1,57 @@
 package JDBC;
 
-public class Mustafas_Task {
+import Utility.DBUtility;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Mustafas_Task extends DBUtility {
+    @Test
+    public void test04() {
+        ArrayList<ArrayList<String>> data =
+                getListData("select d.dept_name, avg(s.salary) as averageSalary from employees.salaries s\n" +
+                        "join employees.dept_emp de on s.emp_no=de.emp_no\n" +
+                        "join employees.departments d on de.dept_no=d.dept_no\n" +
+                        "group by d.dept_name;");
+        for (List<String> row : data) {
+            System.out.println(row);
+        }
+    }
+
+    @Test
+    public void test09() {
+        ArrayList<ArrayList<String>> data = getListData("select d.dept_name, avg(s.salary) as averageSalary from employees.salaries s\n" +
+                "join employees.dept_emp de on s.emp_no=de.emp_no\n" +
+                "join employees.departments d on de.dept_no=d.dept_no\n" +
+                "group by d.dept_name;");
+        for (List<String> row : data) {
+            System.out.println(row);
+        }
+    }
+
+    @Test
+    public void test14() {
+        ArrayList<ArrayList<String>> data = getListData("select e.first_name, e.last_name, max(salary) from employees.employees e\n"
+                + "join employees.dept_emp de on e.emp_no = de.emp_no\n" +
+                "join employees.departments d on de.dept_no = d.dept_no\n" +
+                "join employees.salaries s on e.emp_no = s.emp_no\n" +
+                "where d.dept_name = 'Sales'\n" + "group by e.emp_no\n" +
+                "order by max(salary) desc limit 1;");
+        for (List<String> row : data) {
+            System.out.println(row);
+
+        }
+    }
+
+    @Test
+    public void test19() {
+        ArrayList<ArrayList<String>> data = getListData("select first_name, last_name, hire_date from employees.employees\n" +
+                "where hire_date between '1985-01-01' and '1989-12-31'\n" +
+                "order by hire_date;");
+        for (List<String> row : data) {
+            System.out.println(row);
+        }
+    }
 }
