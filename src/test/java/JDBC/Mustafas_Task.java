b9d7654f05698338b292d68acc52458d0b95f392
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-05-24 15:37:25 +0300

    Projem bitti onaylar mısın?

--- src/test/java/JDBC/Mustafas_Task.java
+++ src/test/java/JDBC/Mustafas_Task.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Mustafas_Task extends DBUtility {
     @Test
-    public void test04() {
+    public void Test04() {
         ArrayList<ArrayList<String>> data =
                 getListData("select d.dept_name, avg(s.salary) as averageSalary from employees.salaries s\n" +
                         "join employees.dept_emp de on s.emp_no=de.emp_no\n" +
@@ -21,7 +21,7 @@ public class Mustafas_Task extends DBUtility {
     }
 
     @Test
-    public void test09() {
+    public void Test09() {
         ArrayList<ArrayList<String>> data = getListData("select d.dept_name, avg(s.salary) as averageSalary from employees.salaries s\n" +
                 "join employees.dept_emp de on s.emp_no=de.emp_no\n" +
                 "join employees.departments d on de.dept_no=d.dept_no\n" +
@@ -32,7 +32,7 @@ public class Mustafas_Task extends DBUtility {
     }
 
     @Test
-    public void test14() {
+    public void Test14() {
         ArrayList<ArrayList<String>> data = getListData("select e.first_name, e.last_name, max(salary) from employees.employees e\n"
                 + "join employees.dept_emp de on e.emp_no = de.emp_no\n" +
                 "join employees.departments d on de.dept_no = d.dept_no\n" +
@@ -46,7 +46,7 @@ public class Mustafas_Task extends DBUtility {
     }
 
     @Test
-    public void test19() {
+    public void Test19() {
         ArrayList<ArrayList<String>> data = getListData("select first_name, last_name, hire_date from employees.employees\n" +
                 "where hire_date between '1985-01-01' and '1989-12-31'\n" +
                 "order by hire_date;");
